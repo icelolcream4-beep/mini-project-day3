bfb863d08ec136bcfa4f51be23e5f89226243953
icelolcream4-beep <icelolcream4@gmail.com>
2026-02-14 11:04:40 -0600

    its like a paint editor now, space to draw, r to change color, and click clears it.

--- project3.js
+++ project3.js
@@ -1,15 +1,25 @@
 function setup() {
-  createCanvas(400, 400);
-  x = 0
+  createCanvas(1200, 1200);
   background(100);
 }
-
+x=0
+y=0
+z=0
 function draw() {
-  
-  noStroke()
-  circle(mouseX,mouseY,100)
+  if (keyIsDown(32) === true) {
+      noStroke()
+      fill(x,y,z)
+      circle(mouseX,mouseY,50)
+}
+
 }
 
 function mousePressed(){
   background(200)
 }
+function keyPressed() {
+  if (key === 'r') {
+    x = random(0,225)
+    y = random(0,225)
+    z = random(0,225)
+}}
